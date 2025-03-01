1e70308e824a518d30a1a3fa27e0cd411d2b193f
Phạm Thị Thuý Huyền <huyenptt34@gmail.com>
2025-03-01 18:33:59 +0700

    Thêm file mới và cập nhật style.css, script.js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2, // Luôn hiển thị 2 slides
+    spaceBetween: 50, // Khoảng cách giữa các slides
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
