7a4a6dbef72413c5f87e8b78930b0a997b9cd453
Phạm Thị Thuý Huyền <huyenptt34@gmail.com>
2025-03-16 05:46:54 +0700

    Cap nhat bai chinh sua

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,1 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 2, // Luôn hiển thị 2 slides
-    spaceBetween: 50, // Khoảng cách giữa các slides
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
+
